efc1bfe1a9b78512e88d4ee0ea4b44e1ad0ec494
Daniel Hodges <hodges.daniel.scott@gmail.com>
2018-01-21 16:53:00 -0500

    Fix doc comments

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 /*
-Package hydrator provides primatives for hydrating struct fields through the
+Package hydrator provides primitives for hydrating struct fields through the
 use of a Hydrator.
 */
 package hydrator
